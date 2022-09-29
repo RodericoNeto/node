61a43b87e9af99ca9df8935c4f1e5df295593dcd
RodericoNeto <talysonsoares40@gmail.com>
2022-09-29 19:45:22 -0300

    set interval e memoria

--- teste.js
+++ teste.js
@@ -1,17 +1,26 @@
 
 // CommonJS -> responsável por importar ou exportar os módulos
+
+
+// console.table(memoria)
+// console.log(`A memória livre é de: ${freeMem}Mb, de um total de ${totalMem}Mb`)
+
+
 const os = require('os')
 
-const totalMem = parseInt(os.totalmem() / 1024 / 1024)
-const freeMem = parseInt(os.freemem() / 1024 / 1024)
+setInterval(() => {
 
-console.log(freeMem, totalMem)
-console.clear
+    const totalMem = parseInt(os.totalmem() / 1024 / 1024)
+    const freeMem = parseInt(os.freemem() / 1024 / 1024)
+    const memLivrePerc = (freeMem / totalMem) * 100
 
-const memoria = {
-    total: `${totalMem} Mb`,
-    free: `${freeMem} Mb`
-}
+    const memoria = {
+        total: `${totalMem} Mb`,
+        free: `${freeMem} Mb`,
+        percentual_livre: `${memLivrePerc.toFixed(2)}%`
+    }
 
-console.table(memoria)
-console.log(`A memória livre é de: ${freeMem}Mb, de um total de ${totalMem}Mb`)
+    console.clear()
+    console.table(memoria)
+    
+}, 1000)
