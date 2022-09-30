921dd85e0f1890f3c86d0ddb721cb4b8ebdf70ac
RodericoNeto <talysonsoares40@gmail.com>
2022-09-29 21:02:08 -0300

    atualizacao

--- teste.js
+++ teste.js
@@ -7,11 +7,15 @@
 
 
 const os = require('os')
+const {byteToGB} = require('../Node-Aula-02/convertUnitStorage')
+const {byteToMB} = require('../Node-Aula-02/convertUnitStorage')
+const fs = require('fs')
 
 setInterval(() => {
 
-    const totalMem = parseInt(os.totalmem() / 1024 / 1024)
-    const freeMem = parseInt(os.freemem() / 1024 / 1024)
+    // const totalMem = parseInt(os.totalmem() / 1024 / 1024)
+    const totalMem = parseInt(byteToMB(os.totalmem()))
+    const freeMem = parseInt(byteToMB(os.freemem()))
     const memLivrePerc = (freeMem / totalMem) * 100
 
     const memoria = {
@@ -22,5 +26,10 @@ setInterval(() => {
 
     console.clear()
     console.table(memoria)
+    fs.appendFile('../Node-Aula-02/log.txt', 
+    'Inserindo log...', 
+    'utf-8', 
+    ()=>console.log('Log inserido')
+    )
     
 }, 1000)
